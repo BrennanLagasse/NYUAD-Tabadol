20786ed43149307278f690d67f0aecc5ca04b135
BrennanLagasse <brennan.lagasse@gmail.com>
2024-04-28 12:02:37 +0400

    Reorganize file structure

--- Vehicle Routing/graph_representation.py
+++ Vehicle Routing/graph_representation.py
@@ -1,6 +1,7 @@
 # Credit to https://github.com/AsishMandoi/VRP-explorations.git
 
 import numpy as np
+import networkx as nx
 
 def generate_vrp_instance(n, seed=None):
 
@@ -20,14 +21,14 @@ def generate_vrp_instance(n, seed=None):
     # Generate VRP instance
     xc = (np.random.rand(n + 1) - 0.5) * 10
     yc = (np.random.rand(n + 1) - 0.5) * 10
-    instance = np.zeros((n + 1, n + 1))
+    cost_matrix = np.zeros((n + 1, n + 1))
     for ii in range(n + 1):
         for jj in range(ii + 1, n + 1):
-            instance[ii, jj] = (xc[ii] - xc[jj]) ** 2 + (yc[ii] - yc[jj]) ** 2
-            instance[jj, ii] = instance[ii, jj]
+            cost_matrix[ii, jj] = (xc[ii] - xc[jj]) ** 2 + (yc[ii] - yc[jj]) ** 2
+            cost_matrix[jj, ii] = cost_matrix[ii, jj]
 
     # Return output
-    return instance, xc, yc
+    return cost_matrix, xc, yc
 
 
 def generate_cvrp_instance(n, m, seed=None):
